203cc70d22b827e1e71d9c92a9d14d3aae681199
Rezgui Med Aziz <rezguiaziz32@gmail.com>
2025-04-14 20:39:28 +0100

    Adding Grasp Low Coupling to Tier services

--- src/main/java/com/C_TechProject/Tier/PersonPhysiqueController.java
+++ src/main/java/com/C_TechProject/Tier/PersonPhysiqueController.java
@@ -14,7 +14,7 @@ import java.util.List;
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
 public class PersonPhysiqueController {
-
+// comment
     public final PersonnePhysiqueService personnePhysiqueService;
 
     @PostMapping("/addperson")
